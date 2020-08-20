2b0a1cb3401dcbbf1fb530b4c96af2da8720802e
Hockey <>
2020-08-20 18:47:33 +0700

    Updates

---
+++ precache-manifest.d91675eb19903bae7e79cd5a180d3d5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ffc65dd34e92f270bd7fce84ad4323",
+    "url": "/tiny-calculator/index.html"
+  },
+  {
+    "revision": "a66997954d8433e7ef22",
+    "url": "/tiny-calculator/static/css/main.85a8031b.chunk.css"
+  },
+  {
+    "revision": "814bda7c2bf844b602c0",
+    "url": "/tiny-calculator/static/js/2.6573ee11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tiny-calculator/static/js/2.6573ee11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a66997954d8433e7ef22",
+    "url": "/tiny-calculator/static/js/main.45b4d13e.chunk.js"
+  },
+  {
+    "revision": "5496aea09d647b7ced63",
+    "url": "/tiny-calculator/static/js/runtime-main.28f838fc.js"
+  }
+]);
